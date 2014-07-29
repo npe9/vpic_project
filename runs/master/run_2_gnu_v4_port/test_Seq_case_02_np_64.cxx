9df8010dd5b497254cc9aa4bc40605985c729294
Dave Nystrom <wdn@lanl.gov>
2014-07-29 14:52:46 -0600

    Reduce number of particles per cell to fit in memory on rzuseq.

--- runs/master/run_2_gnu_v4_port/test_Seq_case_02_np_64.cxx
+++ runs/master/run_2_gnu_v4_port/test_Seq_case_02_np_64.cxx
@@ -233,7 +233,7 @@ begin_initialization {
   // single-processor mesh = 64 * 44 x 44 x 44
 #endif
 
-  double nppc               = 100;     // Ave. number of particles/cell in ea. species
+  double nppc               = 50;      // Ave. number of particles/cell in ea. species
   int load_particles        = 1;       // Flag to turn on/off particle load 
   int mobile_ions           = 0;       // Whether or not to push ions
   double f_He               = 0;       // Ratio of number density of He to total ion density
