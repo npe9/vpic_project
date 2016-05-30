b7ed4cb367cbfd7913856e3f6df6b523e2c88fcc
Dave Nystrom <wdn@lanl.gov>
2016-05-30 13:08:11 -0600

    Add support for running on Trinitite.  Work on changing Denali run support to be like other machines.

--- runs/master/trinitite/lpi_1/run_1_int_v8_port/test_lpi_np_00016.cxx
+++ runs/master/trinitite/lpi_1/run_1_int_v8_port/test_lpi_np_00016.cxx
@@ -127,16 +127,16 @@ begin_initialization {
 
   // 
   double Lx                = 16 * 12.0 * 1e-4;   // In cm (note: 1 micron = 1e-4 cm)   
-  double Ly                =      12.0 * 1e-4;              
-  double Lz                =      12.0 * 1e-4;                 
+  double Ly                =  4 * 12.0 * 1e-4;              
+  double Lz                =  4 * 12.0 * 1e-4;                 
 //???????????????????????????????????????????????????????
   double nx                = 40*16;
-  double ny                = 40;
-  double nz                = 40; 
-  double topology_x        = 16;
-  double topology_y        = 1;
+  double ny                = 40*4;
+  double nz                = 40*4;
+  double topology_x        = 8;
+  double topology_y        = 2;
   double topology_z        = 1;            
-  // single-processor mesh = 16 * 40 x 40 x 40
+  // single-processor mesh = 80 x 80 x 160
 
   double nppc               = 125;     // Ave. number of particles/cell in ea. species
   int load_particles        = 1;       // Flag to turn on/off particle load 
@@ -160,7 +160,7 @@ begin_initialization {
   double uthi_H             = sqrt(t_i/mic2_H);   // vthi/c for H
   double uthi_He            = sqrt(t_i/mic2_He);  // vthi/c for He
 
-  // Plasma skin deptth in cm
+  // Plasma skin depth in cm
   double delta = (vacuum_wavelength / (2*M_PI) ) / sqrt( n_e_over_n_crit ); 
 
   double n_e   = c_vac*c_vac*m_e/(4*M_PI*ec*ec*delta*delta); // electron density in cm^-3
@@ -197,8 +197,8 @@ begin_initialization {
   double nsteps_cycle      = trunc_granular(2*M_PI/(dt*omega),1)+1; 
   dt                       = 2*M_PI/omega/nsteps_cycle; // nsteps_cycle time steps in one laser cycle
 
-  double t_stop            = 101;                 // Runtime in 1/wpe
-  //double t_stop            = 10;                  // Runtime in 1/wpe
+  double t_stop            = 12;                  // Runtime in 1/wpe
+  // double t_stop            = 10;                  // Runtime in 1/wpe
   int particle_interval    = 0; 
   //int poynting_interval    = int(M_PI/((omega+1.5)*dt));     // Num. steps between dumping poynting flux
   int poynting_interval    = 0;                              // Num. steps between dumping poynting flux
@@ -283,11 +283,12 @@ begin_initialization {
   num_step             = int(t_stop/(dt)); 
 
 //??????????????????????????????????????????????????????????
-  status_interval      = 10; 
+//status_interval      = 10;
+  status_interval      = 21;
   sync_shared_interval = status_interval/1;
   clean_div_e_interval = status_interval/1;
   clean_div_b_interval = status_interval/10; 
-  
+
   // Turn off some of the spam
   verbose = 1; 
 
